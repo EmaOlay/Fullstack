1a43ed73240afb19a8394297aa49311341c4acf0
EmaOlay <olayemanuel@gmail.com>
2022-05-18 20:28:02 -0300

    grilla con Vue

--- Scripts/generate_dynamic_grid.js
+++ Scripts/generate_dynamic_grid.js
@@ -9,50 +9,19 @@ let dir2 = "https://api.github.com/repos/EmaOlay/Fullstack/contents/Images/cover
 let data_cod = []
 let mangas = []
 
-axios.get(dir2)
-    .then(
-        response => {
-            //console.log(response.mangas);
-            for (let index = 0; index < response.data.length; index++) {
-                //console.log(response.data[index].name);
-                let codificado = encodeURIComponent(response.data[index].name)
-                data_cod.push(codificado)
-                mangas.push(response.data[index].name)
-
-            }
-            //Ahora que tengo ambos vectores de strings los ordeno
-            data_cod.sort();
-            mangas.sort();
-            //console.log(data);
-            for (let i = 0; i < data_cod.length; i++) {
-
-                chain += `<li class="img_grid_li"><img class="img_grid" src="https://raw.githubusercontent.com/EmaOlay/Fullstack/master/Images/covers/` + data_cod[i] + `" alt="` + mangas[i] + `"></li>`;
-                //console.log(chain);
-            }
-            //lo ingreso al html
-            let img_grid_ul = document.getElementsByClassName('img_grid_ul');
-            img_grid_ul[0].innerHTML = chain;
-            //console.log('Antes del then');
-            
-        }
-    )
-
-    
-/*Termino la carga de la grilla */
-
-
-
 /*Estadisticas*/
 
 let app = Vue.createApp({
 
     data: function(){
         return{
-            Fetched: false,
             counter: -1,
+            info_api:[],
+            total_read:-1,
         }
     },
     methods: {
+        //Traigo la info de las imagenes
         fetchData(url){
             if(url !== ""){
                 fetch(url)
@@ -60,23 +29,38 @@ let app = Vue.createApp({
                 .then(data => {
                     //console.log(data);
                     this.counter= data.length;
-                    this.Fetched = true;
-                    
+                    this.info_api=data;
+                    this.crea_grilla();
                 })
                 .catch(err => console.log(err));
 
             }else{
                 console.log("URL inválida");
             }
+        },
+        //Crea grilla
+        crea_grilla(){
+            this.info_api.sort();
+            
+            for (let i = 0; i < this.info_api.length; i++) {
+
+                chain += `<li class="img_grid_li"><img class="img_grid" src="`+this.info_api[i].download_url+`" alt="` + this.info_api[i].name.split(".")[0] + `"></li>`;
+            }
+            
+            //console.log(chain);
+
+            //lo ingreso al html
+            let img_grid_ul = document.getElementById('img_grid_ul');
+            img_grid_ul.innerHTML = chain;
         }
     },
     created(){
         this.fetchData(dir2);
-
     }
     }
 )
-
 app.mount('#m-counter')
-//app.onMounted(function () => {app.counter = mangas.length;})
+
+
+
 
