1badc3377402d1aa932dc3b4abed5b425dce338c
unknown <olayemanuel@gmail.com>
2022-06-30 18:41:09 -0300

    BD andando

--- Scripts/generate_table.js
+++ Scripts/generate_table.js
@@ -1,10 +1,13 @@
 
 let chain = "";
 
-let dir2 = "https://api.github.com/repos/EmaOlay/Fullstack/contents/Images/covers";
-
+//let dir2 = "https://api.github.com/repos/EmaOlay/Fullstack/contents/Images/covers";
+let dir2 = "http://127.0.0.1:5000/api/mangas/"
 //let data_cod = []
 let data = []
+let nombre=[]
+let l_read = []
+let log_date = []
 axios.get(dir2)
     .then(
         response => {
@@ -13,31 +16,38 @@ axios.get(dir2)
                 //console.log(response.data[index].name);
                 //let codificado = encodeURIComponent(response.data[index].name)
                 //data_cod.push(codificado)
-                data.push(response.data[index].name)
+                nombre.push(response.data[index].name);
+                //console.log(response.data[index].name);
+                l_read.push(response.data[index].last_read);
+                //console.log(response.data[index].last_read);
+                log_date.push(response.data[index].log_date);
+                //console.log(response.data[index].log_date);
 
             }
-            for (let i = 0; i < data.length; i++){
-                //console.log(data[i].split(".")[0]);
-                data[i]=data[i].split(".")[0];
+            /*
+            for (let i = 0; i < response.data.length; i++){
+                
+                console.log(nombre[i]);
+                console.log(l_read[i]);
+                console.log(log_date[i]);
+                
             }
-            //Ahora que tengo ambos vectores de strings los ordeno
-            //data_cod.sort();
-            data.sort();
+            */
             //console.log(data);
-            for (let i = 0; i < data.length; i++) {
+            for (let i = 0; i < response.data.length; i++) {
 
                 chain += `<div class="row">
                 <div class="cell" data-title="Name">
-                    `+data[i]+`
+                    `+nombre[i]+`
                 </div>
                 <div class="cell" data-title="Last Read">
-                    56
+                    `+l_read[i]+`
                 </div>
                 <div class="cell" data-title="URL">
                     <a href="#">Algun Sitio</a>
                 </div>
-                <div class="cell" data-title="Genre">
-                    Algun genero
+                <div class="cell" data-title="Log Date">
+                    `+log_date[i]+`
                 </div>
             </div>`;
                 //console.log(chain);
