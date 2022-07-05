6440684640e45abbcbe2a54ae532f3a33a4305d9
unknown <olayemanuel@gmail.com>
2022-07-05 18:03:39 -0300

    se auto completa el combo box

--- Scripts/upload.js
+++ Scripts/upload.js
@@ -2,13 +2,18 @@
 class Manga {
     Title = "";
     Last_read = 0;
+    id=0;
     //Metodos
-    constructor(title, l_read) {
+    constructor(title, l_read, id) {
         this.Title = title;
         this.Last_read = l_read;
+        this.id = id;
     }
     getInfo() {
-        console.log(this.Title + ' ' + this.Last_read);
+        console.log(this.Title + ' ' + this.Last_read + ' ' + this.id);
+    }
+    to_JSON(){
+        return({"title":this.Title , "last_read":this.Last_read , "id": this.id})
     }
 }
 
@@ -25,8 +30,8 @@ class library {
         this.Mangas = []
     }
     //crea un nuevo manga en la libreria
-    newManga(title, l_read) {
-        let nuevo_manga = new Manga(title, l_read);
+    newManga(title, l_read, id) {
+        let nuevo_manga = new Manga(title, l_read,id);
         this.Mangas.push(nuevo_manga);
         /*Devuelvo la referencia en caso de querer hacer algo
         a continuacion...
@@ -53,6 +58,7 @@ class library {
         } else {
             console.log('No lo encontre genero un nuevo Manga');
         }
+    return this.Mangas[encontrado];
     }
     /*hace update al last_read del manga que me pasen como parametro
     */
@@ -98,42 +104,52 @@ class library {
 const saveForm = document.getElementById("saveForm");
 
 saveForm.addEventListener('click', (e) => {
-    checkInputs();
+    if(checkInputs()!= -2){
+        console.log(Libreria.getMangaInfo(manga.value).to_JSON());
+        envio_manga(Libreria.getMangaInfo(manga.value).to_JSON());
+    }
 })
 
 /*declaro las variables y las funciones para la implementacion */
-let dir2 = "https://api.github.com/repos/EmaOlay/Fullstack/contents/Images/covers";
+//let dir2 = "https://api.github.com/repos/EmaOlay/Fullstack/contents/Images/covers";
+let dir2 = "http://127.0.0.1:5000/api/mangas/"
 let vec_title = [];
 let vec_l_read = [];
+let vec_id=[];
 let Libreria = new library;
 axios.get(dir2)
     .then(
         response => {
             //console.log(response.data);
             for (let index = 0; index < response.data.length; index++) {
-                
-                vec_title.push(response.data[index].name)
-
-            }
-            for (let i = 0; i < vec_title.length; i++) {
-                //console.log(data[i].split(".")[0]);
-                vec_title[i] = vec_title[i].split(".")[0];
-            }
-            //Ahora que tengo el vector de strings lo ordeno
-            vec_title.sort();
-            //pongo numeros al azar del 0 al 200 para las pruebas
-            for (let i = 0; i < vec_title.length; i++) {
-                vec_l_read[i] = Math.floor(Math.random() * 200);
+                vec_title.push(response.data[index].name);
+                vec_l_read.push(response.data[index].last_read);
+                vec_id.push(response.data[index].id);
             }
             /*Por ultimo genero la libreria*/
-            for (let i = 0; i < vec_title.length; i++) {
-                Libreria.newManga(vec_title[i], vec_l_read[i]);
+            for (let i = 0; i < response.data.length; i++) {
+                Libreria.newManga(vec_title[i], vec_l_read[i],vec_id[i]);
             }
-
+            for (let i = 0; i < response.data.length; i++) {
+                var option = document.createElement("option");
+                option.text = vec_title[i];
+                option.value = vec_title[i];
+                manga.add(option);
+            }
+            console.log(Libreria);
         }
     )
 
-
+function envio_manga(json) {
+    let dir3 = "http://127.0.0.1:5000/api/mangas/update"
+    axios.post(dir3, json)
+    .then(function (response) {
+        console.log(response);
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
+}
 
 
 const manga = document.getElementById("manga");
@@ -155,3 +171,6 @@ function checkInputs() {
 
     }
 }
+
+
+
